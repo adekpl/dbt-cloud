bf697137ed83c3edbb0d2f994918d27a2d28eb66
Adrian.pietrzykowski@gmail.com <Adrian.pietrzykowski@gmail.com>
2023-09-01 10:31:32 +0000

    add profit reports

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -9,10 +9,12 @@ select
     c.customername,
     c.segment,
     c.country,
+    c.customerid,
 --from raw prodfuct
     p.category,
     p.productname,
-    p.subcategory
+    p.subcategory,
+    p.productid
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customers') }} as c on c.customerid = o.customerid
 left join {{ ref('raw_products') }} as p on o.productid = p.productid
